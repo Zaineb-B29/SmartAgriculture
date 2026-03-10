f448b0bf97666d028f83fe3c559998447eeb7a30
Zaineb Balegi <balejiizaineb@gmail.com>
2026-03-10 13:57:02 +0100

    Add EmailService interface and EmailServiceImpl class skeleton

---
+++ SmartAgriculture/src/main/java/com/projet/SmartAgriculture/Services/EmailService.java
@@ -0,0 +1,4 @@
+package com.projet.SmartAgriculture.Services;
+
+public interface EmailService {
+}
---
+++ SmartAgriculture/src/main/java/com/projet/SmartAgriculture/Services/EmailServiceImpl.java
@@ -0,0 +1,4 @@
+package com.projet.SmartAgriculture.Services;
+
+public class EmailServiceImpl {
+}
