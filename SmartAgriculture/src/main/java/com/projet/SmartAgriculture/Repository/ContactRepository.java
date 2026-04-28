d0b942a8aa38959e8e75e299d3071889cdb9f41d
Zaineb Balegi <balejiizaineb@gmail.com>
2026-04-29 00:33:20 +0100

    feat: Remove Contact entity, repository, service, and REST controller and replaced it with the message entity

--- SmartAgriculture/src/main/java/com/projet/SmartAgriculture/Repository/ContactRepository.java
+++
@@ -1,8 +0,0 @@
-package com.projet.SmartAgriculture.Repository;
-
-import com.projet.SmartAgriculture.Entity.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactRepository extends JpaRepository<Contact, Long> {
-
-}
